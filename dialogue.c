93d4624c2a7ac0220f38501f7b2c45d7dfb71135
jaykingamez <jaykinchan@gmail.com>
2023-06-14 00:25:15 +0800

    add dialogue

--- dialogue.c
+++ dialogue.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 #include "dialogue.h"
 
-DIALOGUE dialogue[] = {
-    {"ask", "food", "I'm hungry, could I have something to eat?", "Why didn't you say so darling! With passion you forgot to eat, with you joy you forgot your worries! I'm so glad you enjoy me company❤️"},
-    {"ask", "water", "I'm thirsty, could I have something to drink?", "Drink up! Drink until your tipsy! But I hate alcoholics! Please don't be one darling :("}};
+// Ask food, Ask water
+/// 5W1H, give clues
+// Guess character?
+
+DIALOGUE dialogues[] = {
+    {"ask",
+     "food",
+     "I'm hungry, could I have something to eat?", "Why didn't you say so darling! With passion you forgot to eat, with you joy you forgot your worries! I'm so glad you enjoy my company❤️",
+     "Crazy woman"},
+    {"ask",
+     "water",
+     "I'm thirsty, could I have something to drink?", "Drink up! Drink until your tipsy! But I hate alcoholics! Please don't be one darling :(",
+     "I'll even drink to death to get away from you"},
+    {"ask",
+     "what",
+     "What is going on?",
+     "Darling, I have come to protect you. No one else can hurt you here. Far above from the mortal plains, who would dare challenge someone like me?",
+     "Interesting, far above the mortal plain? Is she a Lunarian? Or from heaven? Or even from hell?"},
+    {"ask",
+     "who",
+     "Who are you?",
+     "Darling, wouldn't that ruin the fun? Someone like me might be very lenient, but if you don't know who I am, I will kill you ❤️❤️❤️!",
+     "I sure hope she's joking"},
+    {"ask",
+     "where",
+     "Where am I?",
+     "You're above Earth that's for sure. A place where one might never die. If I wish, you could even live forever, wouldn't that be well? You and I living for all of eternity. :)",
+     "Fuck no get me out of here"},
+    {"ask",
+     "when",
+     "What time is it?",
+     "I don't know the time very well. I usually get my servants like Iku to do it.",
+     "Iku? That narrows down the answer by a lot. It's either Suika, Iku or Tenshi? Or are they pretending to be someone they aren't?"}};
--- dialogue.h
+++ dialogue.h
@@ -1,9 +1,20 @@
 typedef struct dialogue
 {
-  const char *action;
-  const char *tag;
-  const char *dialogue;
-  const char *response;
+  const char *action;   // Eg. Ask
+  const char *tag;      // E.g. food
+  const char *dialogue; // What the player is asking
+  const char *response; // Response from girl
+  const char *comment;  // Comment from player (hinting who the hell the girl is)
 } DIALOGUE;
 
-extern DIALOGUE dialogue[];
+extern DIALOGUE dialogues[];
+
+#define food (dialogues + 0)
+#define water (dialogues + 1)
+#define what (dialogues + 2)
+#define who (dialogues + 3)
+#define where (dialogues + 4)
+#define when (dialogues + 5)
+#define how (dialogues + 6)
+#define why (dialogues + 7)
+#define endOfDialogues (dialogues + 8)
