356c2e35bd4087ae2caf03e28f8b8156a4c3bbd6
jaykingamez <jaykinchan@gmail.com>
2023-06-14 15:46:01 +0800

    make hunger static

--- hunger.c
+++ hunger.c
@@ -7,7 +7,7 @@
 
 // Increase to 100, you die
 // Increase 10 after every conversation
-int hunger = 20;
+static int hunger = 80;
 
 // If return false, not dead
 // If return true, very dead
@@ -30,8 +30,32 @@ bool checkHungerDead()
   return false;
 }
 
+char *hungerDescription()
+{
+  if (hunger >= 80)
+  {
+    return "You might be vegan. But it doesn't matter. If you don't eat anything, you're pretty sure you will die.";
+  }
+  else if (hunger >= 60)
+  {
+    return "Your belly growls, wishing for sustenance. But no sustenance will come, until you ask for it";
+  }
+  else if (hunger >= 40)
+  {
+    return "Eat bugs. Live in pod. That is your life right now. But no bugs to eat.";
+  }
+  else if (hunger >= 20)
+  {
+    return "The delicious food you just ate? Probably in your gut. The thought that you might have consumed human flesh... disturbs you. Youkai girls, am I right lads?";
+  }
+  else if (hunger >= 0)
+  {
+    return "What did you just ate? Who cooked this delicious meal? Or the cook can't cook and just ask someone else to do it for them...";
+  }
+}
+
 void printHunger()
 {
-  printf("\n If hunger reaches %d you die, it increases by %d after every action", FULL, INCREASE);
-  printf("\n Hunger: %d", hunger);
+  printf("Hunger: %d", hunger);
+  printf(hungerDescription());
 }
