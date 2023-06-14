97c67f530751f7763e3c3d1f26862093a4a680c2
jaykingamez <jaykinchan@gmail.com>
2023-06-14 15:48:16 +0800

    update parsexec.c text

--- parsexec.c
+++ parsexec.c
@@ -24,19 +24,21 @@ bool parseAndExecute(char *input)
       printf("\n\nSome of the inhabitants don't get it when you say no. They are Youkai, they aren't used to being rejected.");
       printf("\n\nBut you didn't expect things to turn out like this...");
       printf("\n\nYou don't know what the Youkai did to you, but your head hurts. You don't remember your name...");
-      printf("\n\nA blindfold was put over your eyes, your hands were tied behind your back. You're sitting on something, and I can hear footsteps...\n");
+      printf("\n\nSomething is covering your eyes. Even if you forced them open, an endless void of darkness greeted you. Your hands were tied behind your back, the restraints are biting into your skin. You're sitting on something and based off your posture, you guess it's a chair. You hear footsteps...\n");
       printf("\n\nMysterious girl: \"Ah, you're finally awake.\"");
       printf("\n\nThere was one problem, you don't recognize the woman's voice.");
       printf("\n\nMysterious girl: \"I have told you many times, but it seems you wouldn't listen, making me resort to this. This is all your fault, you understand don't you?\"\n\n");
       printf("\n\nYou struggled to shake your head. You couldn't shake your head, something kept it in place, and your mouth was gagged.\n");
       printf("\n\nMysterious girl: \"Ara ara, I'm so so sorry...\"");
       printf("\n\nThe damn thing was removed from your mouth. You could speak, but what should you say?");
-      printf("\n\nAs stomach growled and your throat was parched with thirst, you realized your starving, and that you're thirsty");
+      printf("\n\nAs your stomach growled and your throat was parched with thirst, you realized your starving, and that you're thirsty");
       printf("\n\nYou didn't feel like chit chatting. Sating your current needs was the goal.");
-      printf("\n\nSyntax Format: Action Topic");
-      printf("\n\nExample: ask food");
-      printf("\n\nExample: ask water");
-      printf("\n\nHint: 5W1H, once you figure who they are, type: ask <character_name>");
+      printf("\n\nYou only had the energy to utter a single word at a time. As your mouth hung open, before closing");
+      printf("\n\nMemories from your time at Ms Keine, flooded your mind, when asking questions one should use 5W1H. You don't know why they appeared, but they seemed useful.");
+      printf("\n\nAnd considering that you're blindfolded, you guess that whoever they are, really wants you to guess their identity.");
+      printf("\n\nSo what to ask?");
+      printf("\n\nfood or water?");
+      printf("\n\nYour mind were only swarmed with thoughts on them.");
       printf("\n");
     }
     else if (strcmp(verb, "ending") == 0)
