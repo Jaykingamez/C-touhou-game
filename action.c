874af900b0a1dbcf0437ab7b76cb6a2147bfadae
jaykingamez <jaykinchan@gmail.com>
2023-06-14 12:32:48 +0800

    Update action.c

--- action.c
+++ action.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include "dialogue.h"
+#include "noun.h"
 
 // edit this later
 void executeAsk(const char *noun)
 {
-  moveObject(getPossession(actorHere(), "ask", noun), player);
+  DIALOGUE *obj = getDialogueOption("what you want to get", noun);
+  if (obj == NULL)
+  {
+    // already handled by getDialogueOption
+  }
+  else
+  {
+    printf("\nYou: %s", obj->dialogue);
+    printf("\nMysterious girl: %s", obj->response);
+    printf("\nYour thoughts: %s", obj->comment);
+    printf("\n");
+  }
 }
